0d0238ee920224e40806ef47a8788f9231e17b09
iShinta <ho_minh_tri@yahoo.fr>
2016-12-05 19:05:58 -0600

    php 3

--- index.php
+++ index.php
@@ -1,94 +1,6 @@
 <!DOCTYPE html>
 
-<?php
-public start(){
-  if(!isset($_COOKIE["id"])){
-    if($_SERVER['REQUEST_METHOD'] === 'POST'){
-      if(isset($_POST["username"])){
-        $username = $_POST["username"];
-        $password = $_POST["password"];
 
-        if($username == "guest" && $password == "dinner"){
-          setcookie("id", $username, time()+3600);
-          setcookie("timeloggedin", time(), time()+3600);
-        }
-      }
-    }else{
-      showLogin();
-    }
-  }else{
-    showSubmit();
-  }
-
-
-}
-
-public showSubmit(){
-  ?>
-  <form>
-    <table style="margin: auto; background-color: lightgrey;">
-      <tr>
-        <td>
-          Name:
-        </td>
-        <td>
-          Items:
-        </td>
-      </tr>
-      <tr>
-        <td>
-          <input type="text" id="user" name="user" />
-        </td>
-        <td>
-          <input type="text" id="items" name="items" />
-        </td>
-      </tr>
-      <tr>
-        <td>
-          <input type="submit" name="submit" />
-        </td>
-        <td>
-          <input type="reset" name="reset" />
-        </td>
-      </tr>
-    </table>
-  </form>
-  <?php
-}
-
-public showLogin(){
-  ?>
-  <form>
-    <table style="margin: auto; background-color: lightgrey;">
-      <tr>
-        <td>
-          Name:
-        </td>
-        <td>
-          <input type="text" id="username" name="username" />
-        </td>
-      </tr>
-      <tr>
-        <td>
-          Items:
-        </td>
-        <td>
-          <input type="text" id="password" name="password" />
-        </td>
-      </tr>
-      <tr>
-        <td>
-          <input type="submit" name="submit" />
-        </td>
-        <td>
-          <input type="reset" name="reset" />
-        </td>
-      </tr>
-    </table>
-  </form>
-  <?php
-}
-?>
 
 <html>
 <head>
@@ -97,6 +9,5 @@ public showLogin(){
 </head>
 
 <body style="background-color: darkgrey;">
-  <?php start(); ?>
 </body>
 </html>
