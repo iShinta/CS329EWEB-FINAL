9f38394f3bfd3222fe2683d8c2acffe24f767ba2
iShinta <ho_minh_tri@yahoo.fr>
2016-12-05 18:48:02 -0600

    First Commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+</head>
+
+<body style="background-color: darkgrey;">
+  <form>
+    <table style="margin: auto; background-color: lightgrey;">
+      <tr>
+        <td>
+          Name:
+        </td>
+        <td>
+          Items:
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <input type="text" id="username" name="username" />
+        </td>
+        <td>
+          <input type="text" id="password" name="password" />
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <input type="submit" name="submit" />
+        </td>
+        <td>
+          <input type="reset" name="reset" />
+        </td>
+      </tr>
+    </table>
+  </form>
+</body>
+</html>
