572c3c0d7dbb709ef35719c16be1517306a453d5
iShinta <ho_minh_tri@yahoo.fr>
2016-12-05 19:06:43 -0600

    php 4

--- index.php
+++ index.php
@@ -1,6 +1,27 @@
 <!DOCTYPE html>
 
+<?php
+public start(){
+  if(!isset($_COOKIE["id"])){
+    if($_SERVER['REQUEST_METHOD'] === 'POST'){
+      if(isset($_POST["username"])){
+        $username = $_POST["username"];
+        $password = $_POST["password"];
 
+        if($username == "guest" && $password == "dinner"){
+          setcookie("id", $username, time()+3600);
+          setcookie("timeloggedin", time(), time()+3600);
+        }
+      }
+    }else{
+    }
+  }else{
+  }
+
+
+}
+
+?>
 
 <html>
 <head>
@@ -9,5 +30,6 @@
 </head>
 
 <body style="background-color: darkgrey;">
+  <?php start(); ?>
 </body>
 </html>
