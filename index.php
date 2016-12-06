2df26653a466866ca8b16f46e9d04412f01e8abf
iShinta <ho_minh_tri@yahoo.fr>
2016-12-05 19:01:22 -0600

    php

--- index.php
+++ index.php
@@ -1,10 +1,30 @@
 <!DOCTYPE html>
 
-<html>
-<head>
-</head>
+<?php
+public start(){
+  if(!isset($_COOKIE["id"])){
+    if($_SERVER['REQUEST_METHOD'] === 'POST'){
+      if(isset($_POST["username"])){
+        $username = $_POST["username"];
+        $password = $_POST["password"];
 
-<body style="background-color: darkgrey;">
+        if($username == "guest" && $password == "dinner"){
+          setcookie("id", $username, time()+3600);
+          setcookie("timeloggedin", time(), time()+3600);
+        }
+      }
+    }else{
+      showLogin();
+    }
+  }else{
+    showSubmit();
+  }
+
+
+}
+
+public showSubmit(){
+  ?>
   <form>
     <table style="margin: auto; background-color: lightgrey;">
       <tr>
@@ -17,7 +37,39 @@
       </tr>
       <tr>
         <td>
+          <input type="text" id="user" name="user" />
+        </td>
+        <td>
+          <input type="text" id="items" name="items" />
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <input type="submit" name="submit" />
+        </td>
+        <td>
+          <input type="reset" name="reset" />
+        </td>
+      </tr>
+    </table>
+  </form>
+  <?php
+}
+
+public showLogin(){
+  <form>
+    <table style="margin: auto; background-color: lightgrey;">
+      <tr>
+        <td>
+          Name:
+        </td>
+        <td>
           <input type="text" id="username" name="username" />
+        </td>
+      </tr>
+      <tr>
+        <td>
+          Items:
         </td>
         <td>
           <input type="text" id="password" name="password" />
@@ -33,5 +85,16 @@
       </tr>
     </table>
   </form>
+}
+?>
+
+<html>
+<head>
+  <title>Potluck Dinner</title>
+  <script src="dinner.js"></script>
+</head>
+
+<body style="background-color: darkgrey;">
+  <?php start(); ?>
 </body>
 </html>
